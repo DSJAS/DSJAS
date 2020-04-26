c1c8ccaca174286837ec96241274d46e078f365f
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-04-26 19:10:39 +0100

    Create settings navbar component
    This is used across all settings panel files

---
+++ public/include/components/AdminSettingsNav.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+Welcome to Dave-Smith Johnson & Son family bank!
+
+This is a tool to assist with scam baiting, especially with scammers attempting to
+obtain bank information or to attempt to scam you into giving money.
+
+This tool is licensed under the MIT license (copy available here https://opensource.org/licenses/mit), so it
+is free to use and change for all users. Scam bait as much as you want!
+
+This project is heavily inspired by KitBoga (https://youtube.com/c/kitbogashow) and his LR. Jenkins bank.
+I thought that was a very cool idea, so I created my own version. Now it's out there for everyone!
+
+Please, waste these people's time as much as possible. It's fun and it does good for everyone.
+
+*/
+
+?>
+
+<nav class="navbar navbar-fluid navbar-expand-lg navbar-dark bg-dark mb-4">
+    <a class="navbar-brand text-light">DSJAS Settings</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="headerNavbar">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link text-primary" href="/admin/settings/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/settings/update.php">Update</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/settings/accounts.php">Accounts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/settings/ext.php">Themes and Plugins</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/settings/mod.php">Modules</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-warning" href="/admin/settings/advanced.php">Advanced</a>
+            </li>
+        </ul>
+    </div>
+</nav>
