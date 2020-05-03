0ff0b794a811eeb3cc29e9ce214f32a8106f736e
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-05-03 12:06:52 +0100

    Implement basic transfer page and logic

---
+++ public/include/Banking.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+Welcome to Dave-Smith Johnson & Son family bank!
+
+This is a tool to assist with scam baiting, especially with scammers attempting to
+obtain bank information or to attempt to scam you into giving money.
+
+This tool is licensed under the MIT license (copy available here https://opensource.org/licenses/mit), so it
+is free to use and change for all users. Scam bait as much as you want!
+
+This project is heavily inspired by KitBoga (https://youtube.com/c/kitbogashow) and his LR. Jenkins bank.
+I thought that was a very cool idea, so I created my own version. Now it's out there for everyone!
+
+Please, waste these people's time as much as possible. It's fun and it does good for everyone.
+
+*/
+
+require_once(ABSPATH . INC . "Users.php");
+require_once(ABSPATH . INC . "DB.php");
+
+
+function performTransaction($sourceAccount, $destAccount, $amount, $description = "Account transfer")
+{
+    // Insert transaction into the transactions table
+    $configuration = parse_ini_file(ABSPATH . "/Config.ini");
+
+    $db_hostname = $configuration["server_hostname"];
+    $db_dbname = $configuration["database_name"];
+    $db_username = $configuration["username"];
+    $db_password = $configuration["password"];
+
+    $database = new DB($db_hostname, $db_dbname, $db_username, $db_password);
+
+    $query = new PreparedStatement(
+        "INSERT INTO `transactions` (`origin_account_id`, `dest_account_id`, `transaction_amount`, `transaction_description`, `transaction_type`) VALUES (?, ?, ?, ?, 'transfer')",
+        [$sourceAccount, $destAccount, $amount, $description],
+        "iids"
+    );
+
+    $database->prepareQuery($query);
+    $database->query();
+
+    // Subtract the balance from origin account
+    $database->clearQuery();
+
+    $query = new PreparedStatement(
+        "UPDATE `accounts` SET `account_balance` = (`account_balance` - ?) WHERE `account_identifier` = ?",
+        [$amount, $sourceAccount],
+        "di"
+    );
+
+    $database->prepareQuery($query);
+    $database->query();
+
+    // Add the balance to destination account
+    $database->clearQuery();
+
+    $query = new PreparedStatement(
+        "UPDATE `accounts` SET `account_balance` = (`account_balance` + ?) WHERE `account_identifier` = ?",
+        [$amount, $destAccount],
+        "di"
+    );
+
+    $database->prepareQuery($query);
+    $database->query();
+}
+
+function userOwnsAccount($accountID, $userID)
+{
+    $configuration = parse_ini_file(ABSPATH . "/Config.ini");
+
+    $db_hostname = $configuration["server_hostname"];
+    $db_dbname = $configuration["database_name"];
+    $db_username = $configuration["username"];
+    $db_password = $configuration["password"];
+
+    $database = new DB($db_hostname, $db_dbname, $db_username, $db_password);
+
+    $query = new SimpleStatement("SELECT `associated_online_account_id` FROM `accounts` WHERE `account_identifier` = $accountID");
+    $database->unsafeQuery($query);
+
+    if (count($query->result) == 0) {
+        return false;
+    } elseif (count($query->result) > 1) {
+        return false;
+    }
+
+    return ($query->result[0]["associated_online_account_id"] == $userID);
+}
---
+++ public/user/Transfer.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+Welcome to Dave-Smith Johnson & Son family bank!
+
+This is a tool to assist with scam baiting, especially with scammers attempting to
+obtain bank information or to attempt to scam you into giving money.
+
+This tool is licensed under the MIT license (copy available here https://opensource.org/licenses/mit), so it
+is free to use and change for all users. Scam bait as much as you want!
+
+This project is heavily inspired by KitBoga (https://youtube.com/c/kitbogashow) and his LR. Jenkins bank.
+I thought that was a very cool idea, so I created my own version. Now it's out there for everyone!
+
+Please, waste these people's time as much as possible. It's fun and it does good for everyone.
+
+*/
+
+require("../include/Bootstrap.php");
+
+require(ABSPATH . INC . "Theme.php");
+require_once(ABSPATH . INC . "Customization.php");
+
+require(ABSPATH . INC . "Users.php");
+require(ABSPATH . INC . "Banking.php");
+
+require_once(ABSPATH . INC . "Util.php");
+
+require(ABSPATH . INC . "csrf.php");
+
+
+if (isset($_GET["performTransfer"])) {
+    $csrf = verifyCSRFToken(getCSRFSubmission($method = "get"));
+    if (!$csrf) {
+        die(getCSRFFailedError());
+    }
+
+    if (!isset($_GET["amount"]) || !isset($_GET["originAccount"]) || !isset($_GET["destinationAccount"])) {
+        header("Location: /user/transfer.php?transferError=1");
+        die();
+    }
+
+    if ($_GET["amount"] == null || $_GET["originAccount"] == null || $_GET["destinationAccount"] == null) {
+        header("Location: /user/transfer.php?transferError=1");
+        die();
+    }
+
+    if (!userOwnsAccount($_GET["originAccount"], getCurrentUserId())) {
+        header("Location: /user/transfer.php?transferError=1");
+        die();
+    }
+
+    if ($_GET["amount"] < 0) {
+        header("Location: /user/transfer.php?transferError=1");
+        die();
+    }
+
+    if (isset($_GET["description"]) && $_GET["description"] != null) {
+        echo ("Description provided!");
+        performTransaction($_GET["originAccount"], $_GET["destinationAccount"], $_GET["amount"], $_GET["description"]);
+    } else {
+        performTransaction($_GET["originAccount"], $_GET["destinationAccount"], $_GET["amount"]);
+    }
+
+    header("Location: /user/transfer.php?transferSuccess=1&originAccount=" . $_GET["originAccount"] . "&amount=" . $_GET["amount"]);
+    die();
+} else {
+    regenerateCSRF();
+}
+
+
+if (!isLoggedIn()) {
+    redirect("/user/Login.php");
+    die();
+}
+
+$config = new Configuration(false, true, false, false);
+
+if (!$config->getKey(ID_THEME_CONFIG, "config", "use_default")) {
+    $currentTheme = $config->getKey(ID_THEME_CONFIG, "extensions", "current_UI_extension");
+} else {
+    $currentTheme = "default";
+}
+
+$theme = new Theme(__FILE__, "user/", $currentTheme);
+$theme->loadTheme();
+$theme->displayTheme();
