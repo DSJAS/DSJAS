f1ece3ab18eece3b22e5d2a2170f5baeeca4bcc0
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-05-05 20:18:52 +0100

    Fix broken links on navbar

--- public/admin/site/UI/default/components/DashboardNav.php
+++ public/admin/site/UI/default/components/DashboardNav.php
@@ -6,7 +6,7 @@ Support navbar component file
 <nav class="navbar navbar-fluid navbar-expand-lg navbar-dark bg-dark text-light">
     <div class="navbar-brand">
         <img src="/assets/logo.png" width="30" height="30" alt="logo">
-        <a class="navbar-brand" href="<?php echo (getBankURL() . "/user/Dashboard.php"); ?>"><?php echo (getBankName() . "<i> - Online</i>"); ?></a>
+        <a class="navbar-brand" href="/user/Dashboard.php"><?php echo (getBankName() . "<i> - Online</i>"); ?></a>
     </div>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@ Support navbar component file
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo (getBankURL() . "/user/Dashboard.php"); ?>">Home</a>
+                <a class="nav-link" href="/user/Dashboard.php">Home</a>
             </li>
 
             <li class="nav-item dropdown">
@@ -32,19 +32,19 @@ Support navbar component file
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo (getBankURL() . "/user/transfer"); ?>">Transfers</a>
+                <a class="nav-link" href="/user/transfer">Transfers</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo (getBankURL() . "/support/Fraud"); ?>">Fraud</a>
+                <a class="nav-link" href="/support/Fraud">Fraud</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo (getBankURL() . "/support/Contact"); ?>">Contact</a>
+                <a class="nav-link" href="/support/Contact">Contact</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo (getBankURL() . "/user/Logout.php"); ?>">Logout</a>
+                <a class="nav-link" href="/user/Logout.php">Logout</a>
             </li>
         </ul>
     </div>
