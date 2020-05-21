283eb623ecd6526b929a4b8ce53795bec65e4f4d
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-05-21 10:20:58 +0100

    Add about pages to default theme
    This includes the main "About us" page and the "Mission statement"
    
    The mission statement is just an XKCD joke policy and the LGPL mixed together.
    
    The about page is just a joke and some stuff talking about fraud
    I've tried to hide as many references to fraud as possible in the site

--- public/admin/site/UI/default/About.php
+++ public/admin/site/UI/default/About.php
@@ -41,5 +41,51 @@ require_once(ABSPATH . INC . "api/theme/Appearance.php");
 // Theme entry point
 function getTheme()
 { ?>
-    <?php require("components/Nav.php"); ?>
+    <?php require("components/Nav.php"); ?> </nav>
+
+    <div class="container-fluid about-page-teaser">
+        <div class="about-page-teaser-overlay rounded">
+            <h1>About us</h1>
+            <br>
+            <h2>Although we're just a bank, we're so much more than a bank</h2>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Our origins</h2>
+        <p>Back in the ancient era of 2007 AD, money was simpler: more elegant. We know, because we were there.
+            Sadly, those days are gone. <?php echo (getBankName()); ?> was created by our three founders to make banking simple again. I guess you could say, make banking great again.
+        </p>
+
+        <h2>Why we care</h2>
+        <p>We care because we've been there. Two of our three founders started out working in mailrooms of their own mansions. Let's just say that they didn't do too well.
+            Before long, they were thrown into financial turmoil as they had to fire themselves for being substandard. Yes, our billionaire founders know what it means to be in financial trouble.
+        </p>
+        <br>
+        <p>That is why we care. We care, because we've been there</p>
+
+        <h2>Our fight on fraud</h2>
+        <p>You may have noticed that many of our online banking pages contain warnings and notices about fraud and scams. That is not an accident.
+            You see, when one of our founders was young, their grandmother was scammed out of 70 cents when they bought a bogus financial product.
+
+            Sadly, she never got back her 70 cents. We vowed to never let anybody go through the trouble that she did. So, we have decided to wage
+            a war on fraud, scams and any kind of sketchy operation that's going on around here. After all, when a red spy makes it into the base,
+            it's up to the blues to protect the briefcase.
+        </p>
+
+        <h2>Get good (at banking)</h2>
+        <p>Our support page and handy articles tell you everything you need to know about banking. Our dedicated team of support agents is available
+            over 2 hours a day! Let's see somebody else give you that.
+
+            To get to our numerous resources, please use the links below.
+        </p>
+
+        <hr>
+
+        <div class="mb-2">
+            <a href="/support/Support" class="btn btn-primary">Visit the support center</a>
+            <a href="/support/Contact" class="btn btn-secondary">Contact us</a>
+            <a href="/support/Fraud" class="btn btn-secondary">Help us fight fraud</a>
+        </div>
+    </div>
 <?php }
---
+++ public/admin/site/UI/default/Mission.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+Welcome to Dave-Smith Johnson & Son family bank!
+
+This is a tool to assist with scam baiting, especially with scammers attempting to
+obtain bank information or to attempt to scam you into giving money.
+
+This tool is licensed under the MIT license (copy available here https://opensource.org/licenses/mit), so it
+is free to use and change for all users. Scam bait as much as you want!
+
+This project is heavily inspired by KitBoga (https://youtube.com/c/kitbogashow) and his LR. Jenkins bank.
+I thought that was a very cool idea, so I created my own version. Now it's out there for everyone!
+
+Please, waste these people's time as much as possible. It's fun and it does good for everyone.
+
+*/
+
+/*
+    DEFAULT THEME - DSJAS
+    =====================
+
+    This is the themeing files included in the default installation of DSJAS.
+    It contains HTML and PHP files required to load and display the default theme.
+
+    This file should never be accessed directly, and instead should only be
+    required by a file which has already bootstrapped the site.
+    This means that your script must have defined the ABSPATH constants
+    and preformed other required bootstrapping tasks before the page
+    can be displayed.
+
+
+    For more information of theming and creating your own themes, please refer to the
+    API documentation for themes and plugins.
+*/
+
+require(ABSPATH . INC . "api/theme/General.php");
+require(ABSPATH . INC . "api/theme/Accounts.php");
+require_once(ABSPATH . INC . "api/theme/Appearance.php");
+
+// Theme entry point
+function getTheme()
+{ ?>
+    <?php require("components/Nav.php"); ?> </nav>
+
+    <div class="container-fluid mission-page-teaser">
+        <div class="mission-page-teaser-overlay rounded">
+            <h1>Our Mission</h1>
+            <br>
+            <h2>It's simple really: Get your money in our bank, no matter what the cost</h2>
+        </div>
+    </div>
+
+    <div class="jumbotron">
+        <h1 class="display-4">Hey there, and welcome to <?php echo (getBankName()); ?>!</h1>
+        <p class="lead">This is our mission statement. You should familiarize yourself with it, because it's very important</p>
+    </div>
+
+    <div class="container">
+        <h2>You and your rights are very important to us. The below is the full text of our mission statement and licence texts</h2>
+
+        <p class="border">
+            <?php echo (file_get_contents(ABSPATH . "/admin/site/UI/" . getCurrentThemeName() . "/components/MissionStatement.html")); ?>
+
+            <hr>
+
+            <?php echo (file_get_contents(ABSPATH . "/admin/site/UI/" . getCurrentThemeName() . "/components/LicenceTexts.txt")); ?>
+
+            <hr>
+
+            <small class="text-small text-muted">Thanks to the XKCD licensing organization and the FSF for the above texts</small>
+        </p>
+    </div>
+<?php }
