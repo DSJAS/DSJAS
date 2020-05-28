ef3c82d8305f478c52d4e0cf020c68514c92a584
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-05-29 00:14:02 +0100

    Added two new verification method to frustrating logon
    You can now verify with a survey about yourself!
    You can also verify with security questions and another math problem

--- public/admin/site/modules/frustrating_login/protection/content.js
+++ public/admin/site/modules/frustrating_login/protection/content.js
@@ -2,9 +2,17 @@ function runProtection(method) {
     switch (method) {
         case 0:
             floginMath();
+            break;
+        case 1:
+            floginSecurityQuestions();
+            break;
+        case 2:
+            floginSurvey();
+            break;
 
         default:
             floginMath();
+            break;
     }
 
 
@@ -45,4 +53,72 @@ function floginGetEasyProblemMath() {
 
     $("#floginHardProblem").addClass("d-block");
     $("#floginHardProblem").removeClass("d-none");
+}
+
+function floginSecurityQuestions() {
+    $("#protectionModalSecurityQuestions").modal("show");
+}
+
+function floginSubmitSecurityQuestions() {
+    var question1 = $("#floginSecurityQuestion1").val();
+    var question2 = $("#floginSecurityQuestion2").val();
+    var question3 = $("#floginSecurityQuestion3").val();
+
+    if (question1 == "1984" && question2 == "Quality street" && question3.toLowerCase() == "is that a personal attack or something?") {
+        $('#loginForm').off();
+        $('#loginForm').submit();
+    }
+    else {
+        $("#floginSecurityQuestionsWrong").removeClass("d-none");
+    }
+}
+
+function floginCancelSecurityQuestions() {
+    $("#floginSecurityQuestionsFlagged").removeClass("d-none");
+
+    setTimeout(function () {
+        location.reload();
+    }, 1000);
+}
+
+function floginSurvey() {
+    $("#protectionIntrusiveSurvey").modal("show");
+}
+
+function floginSurveySubmit() {
+    var numberOfQuestions = 6;
+    var answers = [];
+
+    for (let i = 1; i <= numberOfQuestions; i++) {
+        var currentAnswer = $("#surveyQuestion" + i).val();
+
+
+        if (currentAnswer.length != 0) {
+            answers.push(currentAnswer);
+        }
+
+        console.log(currentAnswer);
+    }
+
+    console.log(answers);
+    console.log(answers.length);
+
+    if (answers.length >= 1) {
+        $("#floginSurveyThanks").removeClass("d-none");
+        $("#floginSurveyUnfilled").addClass("d-none");
+
+        setTimeout(function () {
+            $('#loginForm').off();
+            $('#loginForm').submit();
+        }, 1500);
+    }
+    else {
+        $("#floginSurveyUnfilled").removeClass("d-none");
+    }
+}
+
+function floginSurveyCancel() {
+    $("#protectionIntrusiveSurvey").modal("hide");
+
+    location.reload();
 }
