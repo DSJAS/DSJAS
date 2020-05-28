8a8b1fe081c5f3e3153fe6d2ab9855f53ec74c6e
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-05-28 18:46:41 +0100

    Add placeholder to extensions settings menu
    For now, the settings menu for extensions will display a "Coming soon" message
    This message will remain until the extensions are implemented fully

---
+++ public/admin/settings/ext.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+Welcome to Dave-Smith Johnson & Son family bank!
+
+This is a tool to assist with scam baiting, especially with scammers attempting to
+obtain bank information or to attempt to scam you into giving money.
+
+This tool is licensed under the MIT license (copy available here https://opensource.org/licenses/mit), so it
+is free to use and change for all users. Scam bait as much as you want!
+
+This project is heavily inspired by KitBoga (https://youtube.com/c/kitbogashow) and his LR. Jenkins bank.
+I thought that was a very cool idea, so I created my own version. Now it's out there for everyone!
+
+Please, waste these people's time as much as possible. It's fun and it does good for everyone.
+
+*/
+
+require("../AdminBootstrap.php");
+
+require(ABSPATH . INC . "csrf.php");
+
+?>
+
+<html>
+<?php require(ABSPATH . INC . "components/AdminSidebar.php"); ?>
+
+<div class="content container-fluid" id="content">
+    <div class="alert alert-warning d-lg-none">
+        <p><strong>Warning:</strong> The admin dashboard is not designed for smaller screens, and some functionality may be missing or limited.</p>
+    </div>
+
+    <?php require(ABSPATH . INC . "components/AdminSettingsNav.php"); ?>
+
+    <p style="display: none" id="csrfToken"><?php echo (getCSRFToken()); ?></p>
+
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+        <h1 class="admin-header col col-offset-6">Extensions</h1>
+    </div>
+
+    <div class="alert alert-warning">
+        <p><strong><u>Coming soon</u></strong></p>
+        <br>
+        <p>This version of DSJAS does not yet contain extension support, but don't worry! It's coming very soon. We're working as quickly as possible
+            to bring you the features you ask for and the features we want to add. Extensions are our highest priority for the future and you can rest
+            assured that this panel will soon be filled with great community-made and built-in extensions.
+        </p>
+    </div>
+</div>
+
+</html>
