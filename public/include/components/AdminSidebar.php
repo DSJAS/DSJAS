765ffe77597e6e25621687fcfa31ed9263892ee9
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-05-29 11:02:14 +0100

    Rename information tab to bank tab
    The name was misleading and badly chosen
    The tab will now allow configuration of bank things, like accounts and transactions
    
    Also renamed directory and links to reflect change

--- public/include/components/AdminSidebar.php
+++ public/include/components/AdminSidebar.php
@@ -79,27 +79,30 @@ Please, waste these people's time as much as possible. It's fun and it does good
                 </a>
             </li>
             <li>
-                <a href="#infoSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                    <svg class="bi bi-info-square-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M0 2a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2H2a2 2 0 01-2-2V2zm8.93 4.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                <a href="#bankSubmemu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <svg class="bi bi-shop" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M0 15.5a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zM3.12 1.175A.5.5 0 0 1 3.5 1h9a.5.5 0 0 1 .38.175l2.759 3.219A1.5 1.5 0 0 1 16 5.37v.13h-1v-.13a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.13H0v-.13a1.5 1.5 0 0 1 .361-.976l2.76-3.22z" />
+                        <path d="M2.375 6.875c.76 0 1.375-.616 1.375-1.375h1a1.375 1.375 0 0 0 2.75 0h1a1.375 1.375 0 0 0 2.75 0h1a1.375 1.375 0 1 0 2.75 0h1a2.375 2.375 0 0 1-4.25 1.458 2.371 2.371 0 0 1-1.875.917A2.37 2.37 0 0 1 8 6.958a2.37 2.37 0 0 1-1.875.917 2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.5h1c0 .76.616 1.375 1.375 1.375z" />
+                        <path d="M4.75 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm3.75 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm3.75 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+                        <path fill-rule="evenodd" d="M2 7.846V7H1v.437c.291.207.632.35 1 .409zm-1 .737c.311.14.647.232 1 .271V15H1V8.583zm13 .271a3.354 3.354 0 0 0 1-.27V15h-1V8.854zm1-1.417c-.291.207-.632.35-1 .409V7h1v.437zM3 9.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5V15H7v-5H4v5H3V9.5zm6 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4zm1 .5v3h2v-3h-2z" />
                     </svg>
-                    Information
+                    Bank
                 </a>
-                <ul class="collapse list-unstyled" id="infoSubmenu">
+                <ul class="collapse list-unstyled" id="bankSubmemu">
                     <li>
-                        <a href="/admin/information">General</a>
+                        <a href="/admin/bank">General</a>
                     </li>
                     <li>
-                        <a href="/admin/information/users.php">Users</a>
+                        <a href="/admin/bank/users.php">Users</a>
                     </li>
                     <li>
-                        <a href="/admin/information/accounts.php">Bank accounts</a>
+                        <a href="/admin/bank/accounts.php">Bank accounts</a>
                     </li>
                     <li>
-                        <a href="/admin/information/transactions.php">Transactions</a>
+                        <a href="/admin/bank/transactions.php">Transactions</a>
                     </li>
                     <li>
-                        <a href="/admin/information/support.php">Support requests</a>
+                        <a href="/admin/bank/support.php">Support requests</a>
                     </li>
                 </ul>
             </li>
