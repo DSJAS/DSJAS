b080a177216cb3e65a283c04dffec98db672db50
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-06-06 16:09:09 +0100

    Add coming soon message to statistics menu

--- public/admin/stats.php
+++ public/admin/stats.php
@@ -15,3 +15,21 @@ I thought that was a very cool idea, so I created my own version. Now it's out t
 Please, waste these people's time as much as possible. It's fun and it does good for everyone.
 
 */
+
+require("AdminBootstrap.php");
+
+?>
+
+<div id="content">
+    <?php require(ABSPATH . INC . "components/AdminSidebar.php"); ?>
+
+    <div class="alert alert-warning">
+        <strong>Coming soon</strong>
+
+        <p>Statistics are being worked on as you read this. Please check in soon and update as soon as possible to get access to the statistics of each page,
+            of logins, transactions and more.
+
+            This feature is high priority, expect to see it soon!
+        </p>
+    </div>
+</div>
