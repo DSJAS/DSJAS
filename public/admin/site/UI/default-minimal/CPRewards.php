77e5a67c9c25df20809c3e0258a858c970c3e6f0
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-06-12 11:17:36 +0100

    Fixed default-minimal include bug
    A file which is not present in the minimal version of the theme was being required
    
    This is now fixed and CPRewards.php no longer causes a fatal error

--- public/admin/site/UI/default-minimal/CPRewards.php
+++ public/admin/site/UI/default-minimal/CPRewards.php
@@ -49,7 +49,7 @@ function getTheme()
 { ?>
 
     <body>
-        <?php require(ABSPATH . "/admin/site/UI/default-minimal/components/Nav.php"); ?>
+        <?php require(ABSPATH . "/admin/site/UI/default-minimal/components/DashboardNav.php"); ?>
 
         <div class="container">
             <h1>Oh dear!</h1>
