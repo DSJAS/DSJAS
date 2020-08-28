8ebdb3a7f320ad5f3961d096075ef62fe8bb0184
Ethan Marshall <45273541+OverEngineeredCode@users.noreply.github.com>
2020-08-28 21:35:38 +0100

    Fix for white padding at edges of footer
    The default theme footer should no longer have white bars at each side
    Because of styling problems, the wrong width was being applied

--- public/admin/site/UI/default/components/Footer.php
+++ public/admin/site/UI/default/components/Footer.php
@@ -9,17 +9,17 @@ DSJAS - Default theme
 Footer component file
 -->
 
-<div id="footer">
-    <div class="container-fluid row bg-dark">
-            <div class="col footer-text">
-                <pre class="lead small text-light">&copy 2018 Black Mesa Inc. All rights reserved</pre>
-                <?php addModuleDescriptor("footer");  ?>
-            </div>
-
-            <div class="col footer-links">
-                <a href="/" class="btn btn-primary">Home</a>
-                <a href="/support/Support" class="btn btn-secondary">Help</a>
-                <a href="/support/Contact" class="btn btn-warning">Contact support</a>
-            </div>
+<div id="footer" class="footer bg-dark">
+    <div class="">
+        <div class="footer-text">
+            <pre class="lead small text-light">&copy 2018 Black Mesa Inc. All rights reserved</pre>
+            <?php addModuleDescriptor("footer");  ?>
         </div>
+
+        <div class="footer-links">
+            <a href="/" class="btn btn-primary">Home</a>
+            <a href="/support/Support" class="btn btn-secondary">Help</a>
+            <a href="/support/Contact" class="btn btn-warning">Contact support</a>
+        </div>
+    </div>
 </div>
