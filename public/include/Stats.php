361bdd2add4ce5bab2273388fe0f735552b72a28
Ethan Marshall <ethan.j.marshall@protonmail.com>
2020-12-28 15:09:14 +0000

    Add copyright message to statistics API file

--- public/include/Stats.php
+++ public/include/Stats.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * This file is part of DSJAS
+ * Written and maintained by the DSJAS project.
+ *
+ * Copyright (C) 2020 - Ethan Marshall
+ *
+ * DSJAS is free software which is licensed and distributed under
+ * the terms of the MIT software licence.
+ * Exact terms can be found in the LICENCE file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * above mentioned licence for specific details.
+ */
+
 require_once ABSPATH . INC . "DB.php";
 require_once ABSPATH . INC . "Customization.php";
 
