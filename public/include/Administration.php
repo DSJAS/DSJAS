2c1c8ac850a729f11e22c14c7de31c151dc7759f
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-05-21 21:24:37 +0100

    Fix factory reset for new configuration option

--- public/include/Administration.php
+++ public/include/Administration.php
@@ -120,6 +120,7 @@ function factoryReset(Configuration $config)
     $config->setKey(ID_GLOBAL_CONFIG, "customization", "bank_domain", "https://djohnson.financial");
 
     $config->setKey(ID_GLOBAL_CONFIG, "settings", "allow_access_to_admin", "1");
+    $config->setKey(ID_GLOBAL_CONFIG, "settings", "simulate_missing_nolog_admin", "0");
 }
 
 function installReset(Configuration $config)
