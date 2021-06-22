3996d116c1ccb819cd51ef9e23d4ebe940e2823d
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-06-22 20:59:35 +0100

    Update API copyright message

--- public/include/api/modules/General.js
+++ public/include/api/modules/General.js
@@ -1,3 +1,19 @@
+/*
+ * DSJAS Module API - Client component
+ * Client code component for DSJAS module API system
+ *
+ * Copyright (C) 2020 - Ethan Marshall
+ *
+ * DSJAS is free software which is licensed and distributed under
+ * the terms of the MIT software licence.
+ * Exact terms can be found in the LICENCE file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * above mentioned licence for specific details.
+ */
+
 dsjas.test = function()
 {
   console.log("DSJAS API Loaded");
