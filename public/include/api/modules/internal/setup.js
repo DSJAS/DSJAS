58de8cc7b2adc2579249da8be209f0fe8f0f62f7
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-06-23 07:52:18 +0100

    Implement utility for making POST requests

--- public/include/api/modules/internal/setup.js
+++ public/include/api/modules/internal/setup.js
@@ -6,21 +6,20 @@
 
 const dsjas = {
   accounts: {},
+  login: {},
   util: {}
 }
 
 const api_path = "/include/api/modules/remote/";
 const sess_cookie = "PHPSESSID";
 
-dsjas.util.makeApiRequest = function(api, extract)
-{
+dsjas.util.makeApiRequest = function (api, extract) {
   var result = [];
-  var done = false;
 
   var xmlhttp = new XMLHttpRequest();
   const fullApi = api_path + api + ".php";
 
-  xmlhttp.onreadystatechange = function() {
+  xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
       var response = JSON.parse(this.responseText);
 
@@ -40,9 +39,6 @@ dsjas.util.makeApiRequest = function(api, extract)
 }
 
 dsjas.util.makeGetRequest = function (api, headers) {
-  var result = [];
-  var done = false;
-
   var xmlhttp = new XMLHttpRequest();
   var fullApi = api_path + api + ".php?";
 
@@ -52,4 +48,28 @@ dsjas.util.makeGetRequest = function (api, headers) {
 
   xmlhttp.open("GET", fullApi, true);
   xmlhttp.send();
+}
+
+dsjas.util.makePostRequest = function (api, headers, values) {
+  var result = [];
+
+  var xmlhttp = new XMLHttpRequest();
+  const fullApi = api_path + api + ".php?";
+  var data = "";
+
+  for (var i = 0; i < headers.length; i++) {
+    data += headers[i] + "=" + values[i] + "&";
+  }
+
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      result = JSON.parse(this.responseText);
+    }
+  };
+
+  xmlhttp.open("POST", fullApi, false);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send(data);
+
+  return result;
 }
