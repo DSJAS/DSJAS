0b90602140d91417b36171a9aa8a7f7a0656e0c8
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2023-09-05 14:50:21 +0100

    Don't show local ahead warning if dummy release
    On network or API failure, don't show the version ahead warning.

--- public/admin/settings/update.php
+++ public/admin/settings/update.php
@@ -40,7 +40,8 @@ if (isInsiderBand()) {
 
 function showAheadWarning()
 {
-    if (getCurrentRelease()->laterThanRelease(getLatestAvailableVersion($band)))
+    if (getCurrentRelease()->laterThanRelease(getLatestAvailableVersion($band)) &&
+        !getLatestAvailableVersion($band)->isDummy())
         return "";
     else
         return "d-none";
